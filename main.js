2bb89c58d4a1e8a17a9bc01daf5d692b502024bc
melserngl <nglmercer@gmail.com>
2024-05-15 22:35:13 -0500

    Fix random generation
    # Fix random generation and refactorize
    - random position generation fix and code improvements to be able to modify this
    # ES - arreglo de random posiciones y refactorizacion
    - arreglo de generacion de posiciones aleatorias y mejoras de codigo para poder modificar este

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import './style.scss'
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Crear una matriz de posiciones predefinida
+let positions = [];
+for (let x = 8; x <= 88; x += 4) {
+  for (let y = 8; y <= 88; y += 4) {
+      positions.push({ x, y });
+  }
+}
+
+// Barajar el array de posiciones
+positions = shuffleArray(positions);
+
+// Índice de posición actual
+let currentPositionIndex = 0;
+
+// Función para obtener la siguiente posición de la matriz
+function getNextPosition() {
+  const position = positions[currentPositionIndex];
+  currentPositionIndex = (currentPositionIndex + 1) % positions.length; // Avanzar al siguiente índice circularmente
+  return position;
+}
 var app = document.querySelector('#app');
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -71,114 +99,87 @@ function getMessageHTML(message, { emotes }) {
 }
 
 client.on('message', (channel, tags, message, self) => {
-  console.log(tags)
-  console.log(`Message img ${getMessageHTML(message, tags)}`)
-  console.log(`${tags['display-name']}: ${message}`);
-  
-  var messageBlock = document.createElement('div');
-  messageBlock.id = `message=${tags['id']}`
-/*  messageBlock.innerHTML = `
-  <div class = "title-bar" >
-    <div class = "title-bar-text" >
-    ${tags['display-name']}
-    </div>
+  // console.log(tags);
+// console.log(`Message img ${getMessageHTML(message, tags)}`);
+// console.log(`${tags['display-name']}: ${message}`);
+createMessageWindow(tags, message);
+
+});
+
+function createMessageWindow(tags, message) {
+const messageWindow = document.createElement('div');
+messageWindow.classList.add("window");
+messageWindow.id = `message=${tags['id']}`;
+
+const html = `
+  <div class="title-bar">
+    <div class="title-bar-text">${tags['display-name']}</div>
     <div class="title-bar-controls">
-      <button aria-label="Minimize"> </button>
-      <button aria-label="Maximize"> </button>
-      <button aria-label="Close"> </button>
-      </div>
+      <button aria-label="Minimize"></button>
+      <button aria-label="Maximize"></button>
+      <button aria-label="Close"></button>
+    </div>
   </div>
-  <div class = "window-body">
-    <p> ${message} </p>
+  <div class="window-body">
+    <p>${getMessageHTML(message, tags)}</p>
   </div>
-  `*/
-  var titleBar = document.createElement('div');
-  titleBar.classList.add("title-bar");
-    
-  titleBar.onmousedown = function(event){
-    titleBar.classList.add("active")
-    titleBar.addEventListener("mousemove", onDrag);
-    div.style.zIndex = gz;
-    gz= gz + 1;
-  }
+`;
 
-  titleBar.onmouseup = function(event){
-    titleBar.classList.remove("active");
-    titleBar.removeEventListener("mousemove", onDrag);
-    div.style.zIndex = gz;
-    gz= gz + 1;
-  }
+messageWindow.innerHTML = html;
 
-  titleBar.onmouseleave = function(event){
-    titleBar.classList.remove("active");
-    titleBar.removeEventListener("mousemove", onDrag);
-  }
+const closeButton = messageWindow.querySelector('.title-bar-controls button[aria-label="Close"]');
+closeButton.onclick = function(event){
+  messageWindow.remove();
+};
 
-  var titleBarText = document.createElement('div');
-  titleBarText.innerHTML = `${tags['display-name']}`
-  titleBarText.classList.add("title-bar-text");
+const titleBar = messageWindow.querySelector('.title-bar'); // Seleccionar la barra de título
+var div = app.appendChild(messageWindow);
+let isDragging = false;
+let initialX;
+let initialY;
 
-  var titleBatControls = document.createElement('div');
-  titleBatControls.classList.add('title-bar-controls')
-   
-  var minimizeButton = document.createElement('button');
-  minimizeButton.ariaLabel = "Minimize";
-  titleBatControls.appendChild(minimizeButton);
-
-  var maximizeButton = document.createElement('button');
-  maximizeButton.ariaLabel = "Maximize";
-  titleBatControls.appendChild(maximizeButton);
-
-  var closeButton = document.createElement('button');
-  closeButton.ariaLabel = "Close";
-  closeButton.onclick = function(event){
-    messageBlock.remove();
-  }
-  titleBatControls.appendChild(closeButton);
-  
-
-  titleBar.appendChild(titleBarText);
-  titleBar.appendChild(titleBatControls);
-
-  var windowBody = document.createElement('div');
-  windowBody.classList.add('window-body');
-
-  var messageText = document.createElement('p');
-  messageText.innerHTML = ` ${getMessageHTML(message, tags)} `
-  windowBody.appendChild(messageText);
-
-  messageBlock.appendChild(titleBar);
-  messageBlock.appendChild(windowBody);
-
-  var div = app.appendChild(messageBlock);
-
-  function onDrag({movementX, movementY}){
-    let getStyle = window.getComputedStyle(div);
-    let leftVal = parseInt(getStyle.left);
-    let topVal = parseInt(getStyle.top);
-    div.style.left = `${leftVal + movementX}px`;
-    div.style.top = `${topVal + movementY}px`;
-  }
-
-  var randomtop = Math.floor(Math.random() * window.innerHeight);
-  var randomright = Math.floor(Math.random() * window.innerWidth);
-  if (randomright < window.innerWidth + 250 && randomright > 250)
-    randomright = randomright - 250;
-  if (randomtop > window.innerHeight - messageBlock.clientHeight)
-    randomtop = randomtop - messageBlock.clientHeight
-  div.classList.add("window")
-  div.style.width = "250px"
-  div.style.position = "fixed"
-  div.style.right = randomright + "px"
-  div.style.top = randomtop + "px"
+titleBar.addEventListener('mousedown', (event) => {
+  isDragging = true;
+  initialX = event.clientX - div.getBoundingClientRect().left;
+  initialY = event.clientY - div.getBoundingClientRect().top;
   div.style.zIndex = gz;
-  div.onclick = function(event) {
-    console.log(event);
-    div.style.zIndex = gz;
-    gz= gz + 1;
-  }
   gz = gz + 1;
-  if (app.children.length > urlParams.get("maxmessages")) {
-    app.children[0].remove();
+});
+
+titleBar.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+titleBar.addEventListener('mouseleave', () => {
+  isDragging = false;
+});
+
+titleBar.addEventListener('mousemove', (event) => {
+  if (isDragging) {
+    const newX = event.clientX - initialX;
+    const newY = event.clientY - initialY;
+
+    div.style.left = `${newX}px`;
+    div.style.top = `${newY}px`;
   }
 });
+
+const { x, y } = getNextPosition(); // Obtener la siguiente posición de la matriz
+div.style.right = `${x}%`; // Cambiar a píxeles en lugar de porcentaje
+div.style.top = `${y}%`; // Cambiar a píxeles en lugar de porcentaje
+div.classList.add("window");
+div.style.zIndex = gz;
+
+div.onclick = function(event) {
+  console.log(event);
+  div.style.zIndex = gz;
+  gz = gz + 1;
+}
+
+gz = gz + 1;
+
+if (app.children.length > urlParams.get("maxmessages")) {
+  app.children[0].remove();
+}
+return messageWindow;
+}
